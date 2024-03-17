46c269157877c9e0374c9d6e58e3e37b04498627
Boykaaae <tlxc1224@gmail.com>
2024-03-17 22:43:20 +0800

    1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { findRegion, findRegionName } from "jz-region";
+
+export const region = findRegion;
+export const regionName = findRegionName;
+
+export default {
+	region,
+	regionName
+};
